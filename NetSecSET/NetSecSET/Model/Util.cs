bd1f9847eafd8c1e90d3aa5990d98707133bde87
Danny Sok <evildansta@gmail.com>
2013-05-12 16:57:24 +1000

    Stubbing

---
+++ NetSecSET/NetSecSET/Model/Util.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace NetSecSET.Model
+{
+    class Util
+    {
+        public static Certificate loadCertificate(string fileName)
+        {
+            Certificate cert = null;
+
+            if (File.Exists(fileName))
+            {
+                XmlSerializer s = new XmlSerializer(typeof(Certificate));
+                TextReader tr = new StreamReader(@fileName);
+                cert = (Certificate)s.Deserialize(tr);
+                tr.Close();
+            }
+
+            return cert;
+        }
+
+        public static void saveCertificate(string fileName, Certificate cert)
+        {
+            XmlSerializer s = new XmlSerializer(typeof(Certificate));
+            TextWriter tw = new StreamWriter(@fileName);
+            s.Serialize(tw, cert);
+            tw.Close();
+        }
+
+        public static void Log(string tag, string msg)
+        {
+            TextWriter w = File.AppendText("log.txt");
+            w.Write("\r\nLog Entry : ");
+            w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(),
+                DateTime.Now.ToLongDateString());
+            w.WriteLine("  :");
+            w.WriteLine(" Tag:{0}", tag);
+            w.WriteLine("  :");
+            w.WriteLine("  :{0}", msg);
+            w.WriteLine("-------------------------------");
+        }
+    }
+}
