cd0664fc644aed154b294c8be5418cd6b7907e19
Ed Khou <ed@edkhou.com>
2013-05-13 18:32:48 +1000

    MD5 to be continued

--- NetSecSET/Security/MD5.cs
+++ NetSecSET/Security/MD5.cs
@@ -4,11 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Security.Cryptography;
+using System.IO;
+
 namespace NetSecSET.Security
 {
     class MD5
     {
+        public string getMD5(String input)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(input);
 
+            //int dataByteCount = Encoding.UTF8.GetByteCount(input);
+            //if(dataByteCount % 512 > 0)
+            do
+            {
+                using(MemoryStream ms = new MemoryStream())
+                {
+                    ms.Write(data, 0, 1);
+                }
+            }
+            while((Encoding.UTF8.GetByteCount(input) % 512) > 0);
+
+            
+
+            
+
+
+
+            MD5 md5hash = new MD5.Create();
+            byte[] hashData = md5hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+        }
     }
     
 }
