57830093bc2e126ecdb0915257fea00b19a26bb0
Ed Khou <ed@edkhou.com>
2013-05-19 14:20:34 +1000

    implement hash function

--- NetSecSET/Security/MD5.cs
+++ NetSecSET/Security/MD5.cs
@@ -4,38 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using System.Security.Cryptography;
-using System.IO;
+//using System.Security.Cryptography;
+//using System.IO;
 
 namespace NetSecSET.Security
 {
     class MD5
     {
-        // http://tools.ietf.org/html/rfc1321
-        public string getMD5(String input)
+        //md5 specifications http://tools.ietf.org/html/rfc1321
+        
+        //bernstein constants
+        const uint INITIAL_VALUE = 5381;
+        const uint M = 33;
+
+        //bernstein hash algorithm
+        //uses additions instead of XOR
+        public string getHash(string input)
         {
-            byte[] data = Encoding.UTF8.GetBytes(input);
+            uint hash = INITIAL_VALUE;
+            byte[] bytes = getBytes(input);
 
-            //int dataByteCount = Encoding.UTF8.GetByteCount(input);
-            //if(dataByteCount % 512 > 0)
-            do
+            for (int i = 0; i < bytes.Length; ++i)
             {
-                using(MemoryStream ms = new MemoryStream())
-                {
-                    ms.Write(data, 0, 1);
-                }
+                hash = M * hash + bytes[i];
             }
-            while((Encoding.UTF8.GetByteCount(input) % 512) > 0);
-
-            
-
-            
+            return hash;
 
 
+            //byte[] bytes = Encoding.UTF8.GetBytes(input);
+            ////int dataByteCount = Encoding.UTF8.GetByteCount(input);
+            ////if(dataByteCount % 512 > 0)
+            //do
+            //{
+            //    using (MemoryStream ms = new MemoryStream())
+            //    {
+            //        ms.Write(data, 0, 1);
+            //    }
+            //}
+            //while ((Encoding.UTF8.GetByteCount(input) % 512) > 0);
 
-            MD5 md5hash = new MD5.Create();
-            byte[] hashData = md5hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            //MD5 md5hash = new MD5.Create();
+            //byte[] hashData = md5hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+        }
+
+        public byte[] getBytes(string input)
+        {
+            byte[] bytes = new byte[input.Length * sizeof(char)];
+            System.Buffer.BlockCopy(input.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
         }
     }
-    
 }
